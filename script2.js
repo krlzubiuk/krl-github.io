39cb0274a4322678861400c6a76238cf903209be
krlzubiuk <36868164+krlzubiuk@users.noreply.github.com>
2020-01-25 23:08:33 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+
+    x = prompt("Введите первое число");
+    y = prompt("Введите второе число");
+    var proiz1=x*y;
+
+
+
+    var proiz2=x/y;
+
+
+
+    var proiz3= + x + + y;
+
+
+
+    var proiz4=x-y;
+
+
+
+
+    var proiz5=x*x;
+
+
+alert("умножение  " +proiz1);
+alert("деление  " +proiz2);
+alert("сложение  " +proiz3);
+alert("вычитание  " +proiz4);
+alert("квадрат  " +proiz5);
